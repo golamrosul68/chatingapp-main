0a6e03a05f7ad1c6522296c1b731b0cfcecaca27
golamrosul68 <golamrosul68990@gmail.com>
2025-05-19 19:38:45 +0600

    firebase.config...

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,4 @@
-
 import { initializeApp } from "firebase/app";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyCSw9bUIsNgJqpDvIo43JxE77VUU5wIAYY",
@@ -9,7 +7,7 @@ const firebaseConfig = {
   storageBucket: "chatapp-f91a5.firebasestorage.app",
   messagingSenderId: "661094154596",
   appId: "1:661094154596:web:e8d4fb0ae4b8799cda7a71",
-  measurementId: "G-FXRPLT2QH7"
+  measurementId: "G-FXRPLT2QH7",
 };
 
 // Initialize Firebase
